f8ae4555bdc20e5854ec9dd10c23f5ab3d4b602f
pepsugeki <pumknhd1@gmail.com>
2023-10-10 12:40:55 -0700

    Added Basic Pin Placement Functionality

--- ReactApp/JoggingRouteReactApp/App.js
+++ ReactApp/JoggingRouteReactApp/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useState } from 'react';
+import { StyleSheet, View, Dimensions, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+  const [markers, setMarkers] = useState([]);
+  const [currentCenter, setCurrentCenter] = useState(null);
+
+  const handleRegionChangeComplete = (region) => {
+    setCurrentCenter({
+      latitude: region.latitude,
+      longitude: region.longitude
+    });
+  };
+
+  const handleAddMarker = () => {
+    if (currentCenter) {
+      setMarkers([...markers, currentCenter]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView 
+        style={styles.map} 
+        onRegionChangeComplete={(region) => handleRegionChangeComplete(region)}
+      >
+        {markers.map((marker, index) => (
+          <Marker key={index} coordinate={marker} />
+        ))}
+      </MapView>
+      <View style={styles.buttonContainer}>
+        <Button title="Add Pin" onPress={handleAddMarker} />
+      </View>
     </View>
   );
 }
@@ -20,5 +46,10 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 100,  // position the button 100 pixels up from the bottom
+    alignSelf: 'center'  // center the button horizontally
   },
 });
