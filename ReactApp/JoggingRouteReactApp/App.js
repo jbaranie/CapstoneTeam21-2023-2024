559ff4e9dd04a2cee7fe6f1c6b8ca14e69cb886b
pepsugeki <pumknhd1@gmail.com>
2023-10-10 12:45:32 -0700

    Added "Crosshair" For Pin Placement

--- ReactApp/JoggingRouteReactApp/App.js
+++ ReactApp/JoggingRouteReactApp/App.js
@@ -29,6 +29,13 @@ export default function App() {
           <Marker key={index} coordinate={marker} />
         ))}
       </MapView>
+
+      {/* Crosshair */}
+      <View style={styles.crosshairContainer}>
+        <View style={styles.crossVertical} />
+        <View style={styles.crossHorizontal} />
+      </View>
+
       <View style={styles.buttonContainer}>
         <Button title="Add Pin" onPress={handleAddMarker} />
       </View>
@@ -51,5 +58,28 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 100,  // position the button 100 pixels up from the bottom
     alignSelf: 'center'  // center the button horizontally
+  },
+  crosshairContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: 20,
+    height: 20,
+    marginLeft: -10,
+    marginTop: -10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  crossVertical: {
+    position: 'absolute',
+    height: 20,
+    width: 2,
+    backgroundColor: 'black',
+  },
+  crossHorizontal: {
+    position: 'absolute',
+    width: 20,
+    height: 2,
+    backgroundColor: 'black',
   },
 });
