731304a80d3e97d40ac7216aaf8ad3ad5732c2ff
pepsugeki <pumknhd1@gmail.com>
2023-10-10 11:10:13 -0700

    Added basic map functionality

--- ReactApp/JoggingRouteReactApp/App.js
+++ ReactApp/JoggingRouteReactApp/App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -17,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
