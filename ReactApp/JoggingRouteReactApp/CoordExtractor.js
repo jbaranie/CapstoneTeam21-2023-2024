f55069433ea8ae074906c36f092b70d13a4249f9
lhuan130 <larry.huang@tibitcom.com>
2023-10-21 14:10:34 -0700

    Removed CoordExtractor.js to work within Expo app

--- ReactApp/JoggingRouteReactApp/CoordExtractor.js
+++
@@ -1,5 +0,0 @@
-//Goal: create a tool that can be passed an image link or file and do the following:
-//1. load the file/link image
-//2. verify the file provided is of an accepted type (addendum: identify the type)
-//3. extract geolocation from file; return error if not present
-//4. return geolocation in JS/json object
