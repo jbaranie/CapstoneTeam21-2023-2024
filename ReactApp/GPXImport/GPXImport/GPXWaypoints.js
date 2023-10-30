07696486d1f2142381b8470f1ce3bfbcbc5808bd
pepsugeki <pumknhd1@gmail.com>
2023-10-29 23:04:08 -0700

    Added map centering after location is current

--- ReactApp/GPXImport/GPXImport/GPXWaypoints.js
+++ ReactApp/GPXImport/GPXImport/GPXWaypoints.js
@@ -53,6 +53,10 @@ const GPXWaypoints = () => {
       };
       setUserLocation(userLoc);
       setMapRegion(userLoc);
+
+      if (mapRef.current) {
+        mapRef.current.animateToRegion(userLoc);
+      }
     })();
   }, []);
 
