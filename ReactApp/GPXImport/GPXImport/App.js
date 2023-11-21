8fe47d7ea590edaf77eccbb8e673e6a7572c3b65
lhuan130 <larry.huang@tibitcom.com>
2023-11-21 12:26:22 -0800

    Preview build w/o dev server attempt

--- ReactApp/GPXImport/GPXImport/App.js
+++ ReactApp/GPXImport/GPXImport/App.js
@@ -5,7 +5,6 @@ import GPXWaypoints from './GPXWaypoints';
 import ImageImportDrawer from './ImageImportDrawer';
 import CaptureImageDrawer from './CaptureImageDrawer';
 import GPXFileList from './GPXFileList';
-import 'expo-dev-client';
 
 const Drawer = createDrawerNavigator();
 
