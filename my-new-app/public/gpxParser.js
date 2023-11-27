ffb25e5b452c4fb35264c15eabf396edfd81a0af
jbaranie (Jan Baraniecki) <jbaranie@asu.edu>
2023-11-26 21:56:58 -0900

    Adding Map and GPX import feature into App

---
+++ my-new-app/public/gpxParser.js
@@ -0,0 +1,28 @@
+// gpxParser.js
+
+function parseGPX(gpxData, callback) {
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(gpxData, "text/xml");
+
+    var tracks = [];
+
+    var trkElements = xmlDoc.getElementsByTagName("trk");
+    for (var i = 0; i < trkElements.length; i++) {
+        var track = { points: [] };
+
+        var trksegElements = trkElements[i].getElementsByTagName("trkseg");
+        for (var j = 0; j < trksegElements.length; j++) {
+            var trkptElements = trksegElements[j].getElementsByTagName("trkpt");
+            for (var k = 0; k < trkptElements.length; k++) {
+                var lat = parseFloat(trkptElements[k].getAttribute("lat"));
+                var lon = parseFloat(trkptElements[k].getAttribute("lon"));
+                var point = { lat: lat, lon: lon };
+                track.points.push(point);
+            }
+        }
+
+        tracks.push(track);
+    }
+
+    callback(tracks);
+}
