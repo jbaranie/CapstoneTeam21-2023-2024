866a253f10924a35d9f4b68debe8df14f53fe4d1
EC2 Default User <ec2-user@ip-172-31-31-32.us-east-2.compute.internal>
2023-12-02 04:32:33 +0000

    Adding Raw Files For Easier Duplication on AWS Systems

---
+++ WebpageAppAWSRaw/public/stickyMenu.js
@@ -0,0 +1,21 @@
+ // stickyMenu.js
+ // Visual aethetics code for the top menu in index.html to always be visible up top.
+ //Coder: Jan Baraniecki
+ 
+ //Purpose is to make the Main Menu stay in the top left position as the user scrolls up and down.
+ function makeStickyMenu() 
+ {
+        var menu = document.getElementById("menu");//<div id="menu">
+        var sticky = menu.offsetTop; //The position the Menu should be sticked to.
+     
+    if (window.pageYOffset >= sticky) {
+        menu.classList.add("sticky");//We've scrolled down the webpage, so make it stick
+        } else {
+            menu.classList.remove("sticky");//We've scrolled up the webpage to the original spot or past its starting point.
+        }                    
+ }
+
+// Set up the scroll event listener
+window.onscroll = function() {
+    makeStickyMenu();
+};
