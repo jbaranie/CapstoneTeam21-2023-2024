a9ce57a38ef52a6494b95e033ff7bba8916172c1
jbaranie (Jan Baraniecki) <jbaranie@asu.edu>
2024-02-12 21:54:53 -0900

    Removed html google apps js since its incompatible with React

--- Conversion/cycleapp/src/App.js
+++ Conversion/cycleapp/src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import './gpxParser.js';
 import './styles.css';
+import './GoogleMapAPI.js';
 
 function App() {
   return (
     
     <div className="App">    
-        {/*Background Declarations*/}
+        {/*Variable Declarations*/}
         <input type="file" accept=".gpx" id="fileInput" style={{ display: 'none' }} />
-        {/*Sets up the Main Menu Bar with Styles from styles.cc*/}
+       
+       {/*Sets up the Main Menu Bar with Styles from styles.cc*/}
+       {/*This should be made into a react component since every page has this*/}
         <div id="menu">
             {/* Main Title In Webpage */}
             <h2>The Experimental Route App</h2>
@@ -24,21 +27,20 @@ function App() {
                 <li><a href="#about">About Page</a></li>
             </ul>
         </div>
+        
+        {/*Region Directly Below the Menu Bar*/}
+        <div id="content">
+            {/* WebPage Background Underneath Main Menu Bar */}
+            <section id="main">
+                <div id="background">
+                    <h1>A New Experimental Route App</h1>
+                    <p>Welcome to the Landing Page of our experimental route website. This area will be treated like a dashboard and getting started area.</p> 
+                </div>
+            </section>
+        </div>
       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
     </div>
   );
 }
