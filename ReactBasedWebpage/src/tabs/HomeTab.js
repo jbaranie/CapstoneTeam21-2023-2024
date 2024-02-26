11011d05febb0e8eb486bec6e040481555180c0f
jbaranie (Jan Baraniecki) <jbaranie@asu.edu>
2024-02-25 18:35:09 -0900

    Renaming Files for coherency

---
+++ ReactBasedWebpage/src/tabs/HomeTab.js
@@ -0,0 +1,23 @@
+//CycleAppTab.js
+//This file contains code for the area in index.html when the user hits "Images"
+//It also cotains the code to import an jpeg file.
+//Coder: Jan Baraniecki
+import React, { useState } from 'react';
+import ReactGoogleMapAPI from '../Scripts/ReactGoogleMapAPI';
+
+const HomeTab = () => {
+    return (
+        <div>
+            <section id="main">
+                    <div id="background">
+                        <h1>A New Experimental Route App</h1>
+                        <p>Welcome to the Landing Page of our experimental route website. This area will be treated like a dashboard and getting started area.</p> 
+                         <ReactGoogleMapAPI />
+                    </div>
+            </section>
+           
+        </div>
+    );
+};
+
+export default HomeTab;
