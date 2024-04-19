ec78678d05fb6e15a5e4fa7c0d848034b47d08d1
jbaranie (Jan Baraniecki) <jbaranie@asu.edu>
2024-04-18 22:48:00 -0800

    Fixing Merge Bugs

--- ReactBasedWebpage/src/react_scripts/react_google_map_api.js
+++ ReactBasedWebpage/src/react_scripts/react_google_map_api.js
@@ -8,36 +8,7 @@ import React from 'react';
 import { APIProvider, Map } from '@vis.gl/react-google-maps';
 import Routing from './gpx_routing';
 
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react';
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
-import Routing, { RouteMarkers } from './gpx_routing';
 
-const ReactGoogleMapAPI = (gpxData, gpxCategory) => {
-  //SAMPLE DATA
-  const samplePosition = {lat: 35.1988, lng: -111.652}; //Used to demonstrate basic waypoint marker
-
-  //hooks for things passed between routing component and map sub-components
-  const [markerList, setMarkers] = useState([]);
-  //Must be in a div container so we can set size. Check tab_loader.css
-
-  return (
-    <APIProvider apiKey={process.env.REACT_APP_GOOGLEMAPS_KEY}>
-        
-       
-        <div className="mapContainer">  
-          <Map
-            id="reactGoogleMap" 
-            defaultCenter={samplePosition}
-            defaultZoom={15}
-            /*style={{ width: '75%', height: '500px' }} // Set fixed width and height*/
-          >
-            <RouteMarkers
-              markerInput={markerList}
-            />
-          </Map>
-        </div>  
-=======
 const ReactGoogleMapAPI = ({gpxData, gpxCategory, setMarkers=(data)=>{console.log(data);}}) => {
   //Handles the API key from the web server's environment
   return (
@@ -49,7 +20,6 @@ const ReactGoogleMapAPI = ({gpxData, gpxCategory, setMarkers=(data)=>{console.lo
         defaultZoom={17}
       >
       </Map>
->>>>>>> d8a67e64d8b7c6f52708d6a29f9d74ae641d644e
       <Routing
         gpxData={gpxData}
         gpxCategory={gpxCategory}
